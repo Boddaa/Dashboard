e579a72d8be85c1897bcb50cda2b237c1528fc42
Bodda <abdoesmael599@gmail.com>
2023-01-21 16:17:13 +0200

    upload all files

---
+++ main.js
@@ -0,0 +1,23 @@
+/*
+    -data types 
+    -String 
+    -number
+    -array => object
+    -object
+    -boolean
+*/
+console.log(typeof "abdo esmael" );  // String
+console.log(typeof 5000 ); // number
+console.log(typeof 55.10); // number
+console.log(typeof [10 , 5 , 15]); // array => object
+console.log(typeof ["abdo", "abdo", "bodda"]); // array => object
+console.log(typeof {name: "abdo", age: 20, country: "Eg"} ); // object
+console.log(typeof true); // boolean
+console.log(typeof false); // boolean
+
+/*
+    --variables--
+    -
+*/
+var bodda = "bodda", age = 20;
+console.log(bodda , age)
